d9f663a60133efe9a7d184e7eed752d22432821c
aphkyle <pprotonv@gmail.com>
2021-07-12 23:16:07 +0800

    COLORS

--- snake.py
+++ snake.py
@@ -15,7 +15,7 @@ screen.keypad(1)
 
 curses.start_color()
 
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_RED)
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
 RESET = "\x1B[m"
@@ -59,7 +59,7 @@ def game() -> int:
 
         if remove not in body:
             screen.addch(remove[0], remove[1], ' ', )
-        screen.addch(head[0], head[1], '⬤', curses.color_pair(2))
+        screen.addch(head[0], head[1], '■', curses.color_pair(2))
 
         if action == "d" and direction != 2:
             direction = 0
