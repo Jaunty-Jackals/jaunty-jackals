17c27ee85da39cfbe6655660600b403b0844c06a
ponte-vecchio <haraldur.simonarsson@gmail.com>
2021-07-13 16:08:33 +1000

    hook ConnectFour & run black

--- demo.py
+++ demo.py
@@ -2,28 +2,35 @@ import curses
 import os
 from typing import Any
 
-from bin.utils.palettes import palettes
+# from initload import initialize
+from utils.palettes import palettes
 
 screen = curses.initscr()
-METADATA = {
-    "os": os.name,
-    "term_h_cur": None,
-    "term_h_max": (screen.getmaxyx())[0],
-    "term_h_min": None,
-    "term_w_cur": None,
-    "term_w_max": (screen.getmaxyx())[1],
-    "term_w_min": None,
-}
-
 curses.noecho()
 curses.cbreak()
 curses.start_color()
 curses.can_change_color()
 screen.keypad(1)
 
-# Import a colour palette as desired
-p = palettes.Dracula()  # see bin/utils/palettes/palettes.py
-curclrs = p.alias  # contains the list of colour name aliases
+METADATA = {
+    "os": None,
+    "py_version": None,
+    "term_h_cur": None,
+    "term_h_max": (screen.getmaxyx())[0],
+    "term_h_min": None,
+    "term_w_cur": None,
+    "term_w_max": (screen.getmaxyx())[1],
+    "term_w_min": None,
+    "palette": None,
+}
+
+# run initload
+# METADATA = initialize(METADATA)
+
+# Import a colour palette as desired; see bin/utils/palettes/palettes.py
+METADATA["palette"] = palettes.Commodore64()
+# p = palettes.Gruvbox()
+curclrs = METADATA["palette"].alias  # contains the list of colour name aliases
 
 for i in range(len(curclrs)):
     """COLOUR ORDER AND ALIASES
@@ -39,24 +46,32 @@ for i in range(len(curclrs)):
     black   red     green   yellow  blue    magenta cyan    white
 
     Other
+    16      17
     fg      bg
     """
-    RGB = p.to_rgb(curclrs[i], curses=True)
+    RGB = METADATA["palette"].to_rgb(curclrs[i], curses=True)
     curses.init_color(i, RGB[0], RGB[1], RGB[2])
 
 # Change this to use different colors when highlighting
-curses.init_pair(1, 11, 5)
-h = curses.color_pair(1)  # h is the coloring for a highlighted menu option
-n = curses.A_NORMAL  # n is the coloring for a non highlighted menu option
+curses.init_pair(3, 3, 0)  # menu title
+curses.init_pair(4, 6, 0)  # menu subtitle
+curses.init_pair(16, 10, 0)  # menu highlighted item
+curses.init_pair(17, 7, 0)  # menu normal item
+
+color_title = curses.color_pair(3)
+color_subtitle = curses.color_pair(4)
+selected = curses.color_pair(16)
+idled = curses.color_pair(17)
 
 MENU = "menu"
 COMMAND = "command"
-EXITMENU = "exitmenu"
+QUIT = "exitmenu"
+LOAD = "load"
 
 menu_data = {
     "title": "JackalOS GAME CONSOLE",
     "type": MENU,
-    "subtitle": "",
+    "subtitle": "SELECT",
     "options": [
         {
             "title": "MINE SWEEPER",
@@ -67,6 +82,11 @@ menu_data = {
             "title": "BATTLESHIP",
             "type": COMMAND,
             "command": "venv/bin/python3.9 battleship/client.py",
+        },
+        {
+            "title": "CONNECT FOUR",
+            "type": COMMAND,
+            "command": "venv/bin/python3.9 ConnectFour/blessedConnectFour.py",
         },
         {"title": "CONTENT C", "type": COMMAND, "command": "uqm"},
         {
@@ -99,7 +119,7 @@ menu_data = {
             "options": [
                 {
                     "title": "NO",
-                    "type": EXITMENU,
+                    "type": QUIT,
                 },
                 {"title": "", "type": COMMAND, "command": ""},
                 {"title": "", "type": COMMAND, "command": ""},
@@ -118,13 +138,21 @@ menu_data = {
 }
 
 
-def runmenu(menu: dict, parent: Any) -> Any:
+def wipe(metadata: dict) -> None:
+    """Clears the terminal by sending the os-specific clear command"""
+    if metadata["os"] in "WINDOWS":
+        os.system("CLS")
+    else:
+        os.system("clear")
+
+
+def displaymenu(menu: dict, parent: Any) -> Any:
     """Displays the appropriate menu and returns the option selected"""
     # work out what text to display as the last menu option
     if parent is None:
-        lastoption = "Exit"
+        lastoption = "QUIT CONSOLE"
     else:
-        lastoption = "Return to %s menu" % parent["title"]
+        lastoption = "RETURN"
 
     optioncount = len(menu["options"])  # how many options in this menu
 
@@ -137,33 +165,46 @@ def runmenu(menu: dict, parent: Any) -> Any:
         if pos != oldpos:
             oldpos = pos
             screen.border(0)
-            screen.addstr(2, 2, menu["title"], curses.A_STANDOUT)  # Title for this menu
+            # screen.box(2, 2)
+            # Title
             screen.addstr(
-                4, 2, menu["subtitle"], curses.A_BOLD
-            )  # Subtitle for this menu
+                curses.LINES // 2,  # y
+                curses.COLS // 2 - len(menu["title"]) // 2,  # x
+                menu["title"],
+                curses.color_pair(3),
+            )
+            # Subtitle
+            screen.addstr(curses.LINES - 1, 1, menu["subtitle"], curses.color_pair(4))
 
             # Display all the menu items, showing the 'pos' item highlighted
             for index in range(optioncount):
-                textstyle = n
+                textstyle = idled
                 if pos == index:
-                    textstyle = h
+                    textstyle = selected
                 screen.addstr(
                     5 + index,
                     4,
-                    "%d - %s" % (index + 1, menu["options"][index]["title"]),
+                    "%d - %s"
+                    % (index + 1, menu["options"][index]["title"]),  # TODO: fstring
                     textstyle,
                 )
-            # Now display Exit/Return at bottom of menu
-            textstyle = n
-            if pos == optioncount:
-                textstyle = h
-            screen.addstr(
-                5 + optioncount, 4, "%d - %s" % (optioncount + 1, lastoption), textstyle
-            )
-            screen.refresh()
-            # finished updating screen
 
-        x = screen.getch()  # Gets user input
+            # Now display Exit/Return at bottom of menu
+            textstyle = idled
+            if pos == optioncount:
+                textstyle = selected
+            screen.addstr(
+                5 + optioncount,
+                4,
+                "%d - %s" % (optioncount + 1, lastoption),
+                textstyle,  # TODO: fstring
+            )
+
+            # update screen
+            screen.refresh()
+
+        # Gets user input
+        x = screen.getch()
 
         # What is user input?
         if ord("1") <= x <= ord(str(optioncount + 1)):
@@ -185,42 +226,44 @@ def runmenu(menu: dict, parent: Any) -> Any:
     return pos
 
 
-def processmenu(menu: Any, parent: Any = None) -> Any:
+def processmenu(menu: dict, parent: Any = None) -> None:
     """Calls Showmenu and acts on the selected item"""
     optioncount = len(menu["options"])
     exitmenu = False
-    while not exitmenu:  # Loop until the user exits the menu
-        getin = runmenu(menu, parent)
+
+    while not exitmenu:
+        getin = displaymenu(menu, parent)
+
         if getin == optioncount:
             exitmenu = True
+
         elif menu["options"][getin]["type"] == COMMAND:
-            curses.def_prog_mode()  # save curent curses environment
-            os.system("reset")
-            if menu["options"][getin]["title"] == "Pianobar":
-                os.system(
-                    "amixer cset numid=3 1"
-                )  # Sets audio output on the pi to 3.5mm headphone jack
-            screen.clear()  # clears previous screen
-            os.system(menu["options"][getin]["command"])  # run the command
-            screen.clear()  # clears previous screen on key press and updates display based on pos
+            # save curent curses environment
+            curses.def_prog_mode()
+            wipe(METADATA)
+
+            # clears previous screen
+            screen.clear()
+
+            # run the command
+            os.system(menu["options"][getin]["command"])
+
+            # clear on keypress and update with new position
+            screen.clear()
             curses.reset_prog_mode()  # reset to 'current' curses environment
             curses.curs_set(1)  # reset doesn't do this right
             curses.curs_set(0)
-            os.system(
-                "amixer cset numid=3 2"
-            )  # Sets audio output on the pi back to HDMI
+
         elif menu["options"][getin]["type"] == MENU:
-            screen.clear()  # clears previous screen on key press and updates display based on pos
-            processmenu(menu["options"][getin], menu)  # display the submenu
-            screen.clear()  # clears previous screen on key press and updates display based on pos
-        elif menu["options"][getin]["type"] == EXITMENU:
+            screen.clear()
+            processmenu(menu["options"][getin], menu)
+            screen.clear()
+
+        elif menu["options"][getin]["type"] == QUIT:
             exitmenu = True
 
 
 # Main program
 processmenu(menu_data)
 curses.endwin()  # closes out the menu system and returns you to the bash prompt.
-if METADATA["os"].upper() == "POSIX":
-    os.system("clear")
-elif METADATA["os"].upper() == "NT":
-    os.system("CLS")
+wipe(METADATA)
