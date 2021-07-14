ddf0305bccff2f93798dbd9d4454efc0f7b59f3a
ponte-vecchio <haraldur.simonarsson@gmail.com>
2021-07-14 19:39:18 +1000

    fix flake8 seizure

--- bin/play_sounds/bell.py
+++ bin/play_sounds/bell.py
@@ -1,17 +1,18 @@
 from contextlib import contextmanager
 from typing import ContextManager
 
-
 BELL_CHAR = "\a"
 NO_NEWLINE = ""
 
 
 def bell():
+    """Play a ding sound"""
     print(BELL_CHAR, end=NO_NEWLINE)
 
 
 @contextmanager
 def bell_after(play_bell: bool = True) -> ContextManager:
+    """Play a ding after a task if finished"""
     try:
         yield
 
